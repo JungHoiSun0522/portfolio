c19bde424393298501c541499f269358c3083e4c
JungHoiSun0522 <90242232+JungHoiSun0522@users.noreply.github.com>
2025-04-06 18:23:11 +0900

    .

--- Smart_TV_production_line_automation_factory_solution/server/rests/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import ResponseLog
-
-admin.site.register(ResponseLog)
--- Smart_TV_production_line_automation_factory_solution/server/rests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rests'
