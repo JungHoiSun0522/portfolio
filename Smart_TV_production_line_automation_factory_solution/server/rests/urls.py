c117c0e0fa1dde6ad98bde094335c0c536c17524
JungHoiSun0522 <90242232+JungHoiSun0522@users.noreply.github.com>
2025-04-06 18:26:07 +0900

    .

--- Smart_TV_production_line_automation_factory_solution/server/rests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("robots/", views.log_list),
-    path("robots/<int:log_pk>", views.log_detail),
-    path("robots/last/get", views.last_log_get),
-]
